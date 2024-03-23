d321a82b2778882bb5b3002f2ccfda932bcf5974
PuPuYoman <pupuyoman0725@gmail.com>
2024-03-24 04:34:09 +0800

    first

---
+++ Stegano/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Stegano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 335);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "隱藏";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(410, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(320, 173);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "浮水印";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 173);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "原圖";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 309);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "復原";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "隱藏";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密碼：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 208);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 146);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(113, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(189, 146);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(27, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(320, 173);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "復原圖片";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 22);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(112, 21);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(189, 146);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(284, 246);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 45);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "復原";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "密碼：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(341, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 366);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Stegano";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Stegano/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stegano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 创建 OpenFileDialog 对象
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 设置文件类型过滤
+            openFileDialog.Filter = "图像文件|*.jpg;*.jpeg;*.png;*.gif;*.bmp|所有文件|*.*";
+
+            // 显示 OpenFileDialog 对话框并检查用户是否点击了“确定”按钮
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // 获取用户选择的文件路径
+                    string selectedImagePath = openFileDialog.FileName;
+
+                    // 使用 Image.FromFile 方法加载图像文件
+                    Image selectedImage = Image.FromFile(selectedImagePath);
+
+                    // 根据 PictureBox 控件的大小进行缩放
+                    int pictureBoxWidth = pictureBox1.Width;
+                    int pictureBoxHeight = pictureBox1.Height;
+                    Image resizedImage = selectedImage.GetThumbnailImage(pictureBoxWidth, pictureBoxHeight, null, IntPtr.Zero);
+
+                    // 将缩放后的图像显示在 PictureBox 控件中
+                    pictureBox1.Image = resizedImage;
+
+                    // 显示文件名
+                    string fileName = Path.GetFileName(selectedImagePath);
+                    label3.Text = fileName; // 将 Label 控件的文本更改为文件名
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("加载图像失败: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 创建 OpenFileDialog 对象
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 设置文件类型过滤
+            openFileDialog.Filter = "图像文件|*.jpg;*.jpeg;*.png;*.gif;*.bmp|所有文件|*.*";
+
+            // 显示 OpenFileDialog 对话框并检查用户是否点击了“确定”按钮
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // 获取用户选择的文件路径
+                    string selectedImagePath = openFileDialog.FileName;
+
+                    // 使用 Image.FromFile 方法加载图像文件
+                    Image selectedImage = Image.FromFile(selectedImagePath);
+
+                    // 根据 PictureBox 控件的大小进行缩放
+                    int pictureBoxWidth = pictureBox2.Width;
+                    int pictureBoxHeight = pictureBox2.Height;
+                    Image resizedImage = selectedImage.GetThumbnailImage(pictureBoxWidth, pictureBoxHeight, null, IntPtr.Zero);
+
+                    // 将缩放后的图像显示在 PictureBox 控件中
+                    pictureBox2.Image = resizedImage;
+
+                    // 显示文件名
+                    string fileName = Path.GetFileName(selectedImagePath);
+                    label4.Text = fileName; // 将 Label 控件的文本更改为文件名
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("加载图像失败: " + ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 创建 OpenFileDialog 对象
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 设置文件类型过滤
+            openFileDialog.Filter = "图像文件|*.jpg;*.jpeg;*.png;*.gif;*.bmp|所有文件|*.*";
+
+            // 显示 OpenFileDialog 对话框并检查用户是否点击了“确定”按钮
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // 获取用户选择的文件路径
+                    string selectedImagePath = openFileDialog.FileName;
+
+                    // 使用 Image.FromFile 方法加载图像文件
+                    Image selectedImage = Image.FromFile(selectedImagePath);
+
+                    // 根据 PictureBox 控件的大小进行缩放
+                    int pictureBoxWidth = pictureBox3.Width;
+                    int pictureBoxHeight = pictureBox3.Height;
+                    Image resizedImage = selectedImage.GetThumbnailImage(pictureBoxWidth, pictureBoxHeight, null, IntPtr.Zero);
+
+                    // 将缩放后的图像显示在 PictureBox 控件中
+                    pictureBox3.Image = resizedImage;
+
+                    // 显示文件名
+                    string fileName = Path.GetFileName(selectedImagePath);
+                    label5.Text = fileName; // 将 Label 控件的文本更改为文件名
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("加载图像失败: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 检查 textBox1 是否有输入密码
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                // 显示提示消息
+                MessageBox.Show("請輸入密碼在進行此操作！");
+            }
+            else
+            {
+                // 检查 pictureBox1 和 pictureBox2 是否都有图像
+                if (pictureBox1.Image != null && pictureBox2.Image != null)
+                {
+                    // 当两个 pictureBox 中都有图像时，执行你的逻辑
+                    // 例如：合并两个图像
+                }
+                else
+                {
+                    // 提示用户需要先放入两张图像才能执行操作
+                    MessageBox.Show("請放入圖片在進行此操作!");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // 检查 textBox1 是否有输入密码
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                // 显示提示消息
+                MessageBox.Show("請輸入密碼在進行此操作！");
+            }
+            else
+            {
+                if (pictureBox3.Image != null)
+                {
+                    // 当 pictureBox3 中有图像时，执行你的逻辑
+                    // 例如：处理 pictureBox3 中的图像
+                }
+                else
+                {
+                    // 提示用户需要先放入图像才能执行操作
+                    MessageBox.Show("請放入圖片在進行此操作!");
+                }
+            }
+        }
+    }
+}
